890520e4f2dec79e8a0358cd66fde4a8ea42813e
CodingSaint1984 <fengshaoting@sjtu.edu.cn>
2022-03-06 14:00:38 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,64 @@
+// To convert a decimal value to its equal fraction expression
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int temp;
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    int part1, repetend, flag, temp, counter1 = 0, counter2 = 0, numerator, denominator, GCD;
+    printf("if contain non-repetend part:\n");
+    scanf("%d", &flag); // if includes several numbers(part1) in front of repetend, flag = 1; else, flag = 0
+    if (flag) {
+        printf("non-repetend part:\n");
+        scanf("%d", &part1);
+        // how many digits of part1
+        temp = part1;
+        while (temp > 10) {
+            temp /= 10;
+            counter1++;
+        }
+        counter1++;
+    }
+    printf("repetend part:\n");
+    scanf("%d", &repetend);
+    // how many digits of repetend
+    temp = repetend;
+    while (temp > 10) {
+        temp /= 10;
+        counter2++;
+    }
+    counter2++;
+    if (flag) {
+        numerator = part1 * pow(10, counter2) + repetend - part1;
+        for (; counter2--; counter2 > 0) {
+            denominator *= 10;
+            denominator += 9;
+        }
+        denominator *= pow(10, counter1);
+    }
+    else {
+        numerator = repetend;
+        for (; counter2--; counter2 > 0) {
+            denominator *= 10;
+            denominator += 9;
+        }
+    }
+    GCD = gcd(denominator, numerator);
+    printf("numerator: %d\n", numerator / GCD);
+    printf("denominator: %d\n", denominator / GCD);
+    return 0;
+}
+
+
